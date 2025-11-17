d218c0d84a3549e37c5758ee84a2f1fee4a25fc4
fasih-r3500 <fasih.r3500@gmail.com>
2025-11-17 22:48:02 +0000

    Fix and update issues with building

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-    ],
+    presets: ['babel-preset-expo'],
     plugins: [
-      "nativewind/babel",       // Nativewind plugin
-      "react-native-reanimated/plugin"  // Reanimated plugin
+      'react-native-reanimated/plugin', // Reanimated plugin MUST be last
     ],
   };
 };
