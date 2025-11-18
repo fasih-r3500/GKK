56c13982764dafcddb1ed0a18f873e52b419d347
Fasih <fasih.r3500@gmail.com>
2025-11-18 00:31:07 +0000

    chore: update dependencies in package.json
    - Added react-native-css-interop version ^0.2.1
    - Upgraded react-native-reanimated to version ~4.1.1
    - Upgraded react-native-safe-area-context to version ~5.6.0

--- babel.config.js
+++ babel.config.js
@@ -3,10 +3,7 @@ module.exports = function (api) {
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-    ],
-    plugins: [
-      "nativewind/babel",       // Nativewind plugin
-      "react-native-reanimated/plugin"  // Reanimated plugin
+      "nativewind/babel",
     ],
   };
 };
